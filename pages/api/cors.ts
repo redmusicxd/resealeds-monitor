375ed571283054bd0a9543a94b54b020eeae23f3
White Red <dannegru40@gmail.com>
2023-03-23 15:19:32 +0200

    cors change

--- pages/api/cors.ts
+++ pages/api/cors.ts
@@ -13,15 +13,34 @@ export default async function handler(
 ) {
   try {
     const url = req.body?.url;
+    const host = req.headers.host
 
-    const {data, headers} : {data: string, headers: {[k: string]: string[]}} = await client.get(url, {
+    let req_headers = req.headers;
+
+    delete req_headers.accept;
+    delete req_headers['content-type'];
+    delete req_headers['content-length'];
+    delete req_headers.origin;
+    delete req_headers.referer;
+    delete req_headers.host;
+
+    const { data, headers, config }: { data: string, headers: { [k: string]: string[] }, config: any } = await client.get(url, {
       headers: {
-        "User-Agent": req.headers['user-agent']
+        ...req_headers,
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Cookie": req.headers.cookie,
+        "User-Agent": req.headers['user-agent'],
+        "upgrade-insecure-requests": 1,
+        "sec-fetch-user": "?1",
+        "sec-fetch-site": "none",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-dest": "document"
       }
-    }) 
-    res.writeHead(200, {"set-cookie": headers['set-cookie']?.map(a => a.replace(".emag.ro", req.headers.host!.replace(":3000", "")).replace("secure;", ""))}).end(data)
+    })
+
+    res.writeHead(200, {...headers, "set-cookie": headers['set-cookie']?.map(a => a?.replace(".emag.ro", host!?.replace(":3000", ""))?.replace("secure;", "")) }).end(data)
     // res.send(data)
-      // res.write(text);
+    // res.write(text);
   } catch (error) {
     console.log(error);
     res.status(500).end("500 Internal Server Error")
